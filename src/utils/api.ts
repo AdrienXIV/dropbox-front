e686e1d8105d0ae224991ceb41b6067ea5e4010c
adrien <adri_00@hotmail.fr>
2020-11-17 18:07:42 +0100

    📦 NEW: ajout d'un fichier api pour écrire toutes les requetes + initialisation d'axios

---
+++ src/utils/api.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { URL_API } from '../info';
+
+// définition de l'url de l'api comme base afin d'éviter de la réecrire
+axios.defaults.baseURL = URL_API;
+
+/**
+ * GET
+ */
+
+/**
+ * POST
+ */
+
+/**
+ * PATCH
+ */
+
+/**
+ * DELETE
+ */
