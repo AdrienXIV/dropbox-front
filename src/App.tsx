a387d5737441455fb0347f7928e89944bcd5e807
adrien <adri_00@hotmail.fr>
2020-11-17 17:02:36 +0100

    📦 NEW: ajout d'une route protégée dans app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import Header from './components/Header';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import Register from './pages/Register';
 import Home from './pages/Home';
 
 import history from './history';
 
+const isConnected = false;
+
+// si la personne n'est pas connectée, on la redirige vers l'inscription
+const ProtectedRoute = ({ ...props }) => (!isConnected ? <Redirect to='/inscription' /> : <Route {...props} />);
+
 function App(): JSX.Element {
   return (
     <div className='App'>
       <Router history={history}>
-        <Header />
         <Switch>
-          <Route exact path='/' component={Home} />
           <Route exact path='/inscription' component={Register} />
+
+          {/* ROUTES NECESSITANT D'ETRE CONNECTE */}
+          <ProtectedRoute exact path='/' component={Home} />
         </Switch>
       </Router>
     </div>
