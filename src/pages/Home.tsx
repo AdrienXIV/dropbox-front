c8f05a2dfe9dd2e73ee6a34eaa87201302934082
adrien <adri_00@hotmail.fr>
2020-11-18 17:27:07 +0100

    👌 IMPROVE: suppression dossier loginform

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
 import HomeGrid from '../components/HomeGrid';
 
 export default class Home extends React.Component {
