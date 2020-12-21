7d18dfb2c351d7031599712e2e3598eb6774e16e
adrien <adri_00@hotmail.fr>
2020-12-21 19:00:23 +0100

    📦 NEW: détecter et enregistrer sql

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Snackbar, withStyles, WithStyles } from '@material-ui/core';
 import styles, { Styles } from './styles';
+import 'codemirror/addon/lint/lint.css';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/css/css';
+import 'codemirror/mode/sql/sql';
+import 'codemirror/addon/lint/lint';
+import 'codemirror/addon/lint/json-lint';
 import { Controlled as ControlledEditor } from 'react-codemirror2';
-import { js_beautify, html_beautify, css_beautify } from 'js-beautify';
 import beautify from 'beautify';
+import sqlFormatter from 'sql-formatter';
 import { saveCodeFile } from '../../utils/api';
 import history from '../../history';
 import Alert, { AlertProps } from '@material-ui/lab/Alert';
+
 interface P {
   language: string;
   refValue: React.RefObject<any> | undefined;
@@ -47,12 +52,19 @@ export default class Editor extends React.Component<P & WithStyles<Styles>, S> {
   };
 
   beautify = () => {
+    console.log('beautify', this.state.langage);
     switch (this.state.langage) {
       case 'xml':
         this.setState({ code: beautify(this.state.code, { format: 'xml' }) });
         break;
       case 'html':
         this.setState({ code: beautify(this.state.code, { format: 'html' }) });
+        break;
+      case 'json':
+        this.setState({ code: beautify(this.state.code, { format: 'json' }) });
+        break;
+      case 'sql':
+        this.setState({ code: sqlFormatter.format(this.state.code) });
         break;
       default:
         break;
